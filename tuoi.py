241763f3e703beb3a9b8701411747f0654ffd42c
hieeeu🇻🇳 <nguyenminhhieu091012@gmail.com>
2026-05-30 07:31:56 +0700

    Update tuoi.py for better "print()"

--- tuoi.py
+++ tuoi.py
@@ -1,12 +1,12 @@
-age = input("Bạn năm nay bao nhiêu tuổi? :"
+age = input("Bạn năm nay bao nhiêu tuổi? :")
 
-if age < 10:
+if int(age) < 10:
 	print("Bạn là thằng con nít")
-elif age < 18:
+elif int(age) < 18:
 	print("Bạn là trẻ trâu")
-	if age >= 5 and age <= 17:
+	if int(age) >= 5 and int(age) <= 17:
 		print("Bạn là trẻ nghé")
-elif age >= 18 and age <= 50:
+elif int(age) >= 18 and int(age) <= 50:
 	print("Bạn là người lớn")
 else:
 	print("Bạn là người già")
