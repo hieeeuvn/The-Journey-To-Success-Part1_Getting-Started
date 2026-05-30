ff1d6029498b83ccc44861ae543355a2e39bb614
hieeeu🇻🇳 <nguyenminhhieu091012@gmail.com>
2026-05-30 08:51:41 +0700

    Add old activities

---
+++ bai tap python/bai3.py
@@ -0,0 +1,3 @@
+c = int(input("nhập độ c : "))
+f = c * 33.8
+print(str(c) + " độ c = " + str(f) + " độ f")
---
+++ bai tap python/bai4.py
@@ -0,0 +1,6 @@
+a = int(input("Nhập vào một số nguyên a: "))
+
+if a % 2 == 0:
+  print(True)
+else:
+  print(False)
